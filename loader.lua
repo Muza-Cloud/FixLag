46dbc91a093294b3d1bc5a0efddbec71928e1247
Muza-Cloud <m7za@protonmail.com>
2026-09-25 21:13:19 +0500

    Fix game resource URLs in loader.lua
    Updated URLs for game resources in loader.lua.

--- loader.lua
+++ loader.lua
@@ -1,9 +1,9 @@
 if _G.M7ZA_LoaderRan then return end
 _G.M7ZA_LoaderRan = true
 
-local games = {
-    [10449761463] = "https://raw.githubusercontent.com/m7za1/fixlagbym7za/refs/heads/main/tsbg.lua",
-    [15269951959] = "https://raw.githubusercontent.com/m7za1/fixlagbym7za/refs/heads/main/lbg.lua"
+local games = 
+    [10449761463] = "https://raw.githubusercontent.com/m7za/FixLag/refs/heads/main/tsbg.lua",
+    [15269951959] = "https://raw.githubusercontent.com/m7za1/FixLag/refs/heads/main/lbg.lua"
 }
 
 local url = games[game.PlaceId]
