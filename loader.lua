e2bf36a8dcc3be24a5e5e9fd3c6c0cb873ff0775
Muza-Cloud <m7za@protonmail.com>
2026-09-25 23:38:47 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,8 +2,8 @@ if _G.M7ZA_LoaderRan then return end
 _G.M7ZA_LoaderRan = true
 
 local games = 
-    [10449761463] = "https://raw.githubusercontent.com/m7za/FixLag/refs/heads/main/tsbg.lua",
-    [15269951959] = "https://raw.githubusercontent.com/m7za/FixLag/refs/heads/main/lbg.lua"
+    [10449761463] = "https://raw.githubusercontent.com/Muza-Cloud/FixLag/refs/heads/main/tsbg.lua",
+    [15269951959] = "https://raw.githubusercontent.com/Muza-Cloud/FixLag/refs/heads/main/lbg.lua"
 }
 
 local url = games[game.PlaceId]
